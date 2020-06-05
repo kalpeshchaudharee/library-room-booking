45667b70c5a217f1c32267d20acd7c13823f026e
kalpeshchaudharee <kalpeshchaudhari893@gmail.com>
2020-06-05 17:38:22 +0530

    booking room in library

---
+++ library-room-booking/database/migrations/2020_06_04_144953_create_bookings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('room_id');
+            $table->timestamp('start_time')->nullable();
+            $table->timestamp('end_time')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('room_id')->references('id')->on('rooms');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
---
+++ library-room-booking/routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('login', 'API\UserController@login');
+Route::post('forgot_password', 'API\UserController@forgotPassword');
+Route::post('reset_password', 'API\UserController@resetPassword');
+Route::group(['middleware' => 'auth:api'], function(){
+    Route::get('rooms', 'API\RoomController@roomList');
+    Route::get('room/{id}', 'API\RoomController@roomDetails');
+    Route::post('room/booking/{roomId}', 'API\BookingController@createBooking');
+});
